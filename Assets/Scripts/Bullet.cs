652e4b1ca063e45afc5dc8067f720bf94659be8c
DESKTOP-R12LSJN\KWC <wc5427@naver.com>
2025-06-13 20:53:04 +0900

    몬스터 충돌 버그 수정

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -26,8 +26,6 @@ public class Bullet : MonoBehaviour
 
     }
 
-
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Enemy") && collision.transform == target)
