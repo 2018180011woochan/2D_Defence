90f8bc0b32de9984947b44fe4ef0887cc1dc7914
DESKTOP-R12LSJN\KWC <wc5427@naver.com>
2025-06-19 21:01:10 +0900

    룰렛 애니메이션 생성

---
+++ Assets/Scripts/GambleManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class GambleManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
